222b018f3631530bbbe7231856c845d498b2e26f
Kamal Waila <kamal.waila@corteva.com>
2020-06-12 22:01:27 +0530

    Updates

---
+++ precache-manifest.b62fafa1d5f439cad3e1c01fa5c28c7b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0266d157dafab82e00fb97db4e5e81c5",
+    "url": "/PhotoLibrary/index.html"
+  },
+  {
+    "revision": "ec4860eb020d01151448",
+    "url": "/PhotoLibrary/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b86ee50b49c200f37079",
+    "url": "/PhotoLibrary/static/css/main.9f25efde.chunk.css"
+  },
+  {
+    "revision": "ec4860eb020d01151448",
+    "url": "/PhotoLibrary/static/js/2.543d9af3.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/PhotoLibrary/static/js/2.543d9af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86ee50b49c200f37079",
+    "url": "/PhotoLibrary/static/js/main.6b9683ee.chunk.js"
+  },
+  {
+    "revision": "9d53bd4edde23f4179c0",
+    "url": "/PhotoLibrary/static/js/runtime-main.c287330c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PhotoLibrary/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/PhotoLibrary/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/PhotoLibrary/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/PhotoLibrary/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/PhotoLibrary/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/PhotoLibrary/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/PhotoLibrary/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/PhotoLibrary/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/PhotoLibrary/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/PhotoLibrary/static/media/poster9.65d6d500.jpg"
+  }
+]);
